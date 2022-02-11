ddde5ec568cb2d8f017359c1238d40606eab9906
Paul <paulmichaelhardman@gmail.com>
2022-02-11 10:22:07 +0100

    added text SVG

---
+++ dist/nav.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function classToggle() {
+  var navs = document.querySelectorAll('.Navbar__Items');
+  navs.forEach(function (nav) {
+    return nav.classList.toggle('Navbar__ToggleShow');
+  });
+}
+
+document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
